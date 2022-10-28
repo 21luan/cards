027a60eb99a50abd3673ddbab12bb637229ced7c
Luan Carvalho <luancarvalhoramosdealmeida@gmail.com>
2022-10-27 22:34:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("fn")
+
+button.addEventListener("click", click)
+
+function click (){
+    alert("A fullmind é uma marca de roupas expecializada na produção de camisetas da moda estilo street!")
+}
